31646aaa314adc7c76e93a14a76579e82aed7576
Cristopher Masserini <61479774+CristopherMasserini@users.noreply.github.com>
2020-07-21 17:23:12 -0400

    Update Graph_file_creation.py

--- Graph_file_creation.py
+++ Graph_file_creation.py
@@ -6,7 +6,7 @@ link = 'https://1984.sh/covid19-domains-feed.txt'
 f = requests.get(link)
 link_text = f.text.split('\n')
 
-seconds_of_record = list()  # The date and time of the hostname creation using Epoch time
+date_of_record = list()  # The date and time of the hostname creation using Epoch time
 hostnames = list()  # The hostname
 start_index = link_text.index("date,time,hostname")
 for record in link_text[start_index + 1:]:
@@ -17,17 +17,13 @@ for record in link_text[start_index + 1:]:
         hostname = record_list[2]  # The host name
         record_datetime = datetime.strptime(record_datetime_str, '%Y-%m-%d %H:%M:%S.%f')  # Converts to datetime
         if record_datetime_str != "2020-03-14 15:11:20.684411":
-            day = record_datetime.day
-            mon = record_datetime.month
-            year = record_datetime.year
-            date = str(year) + "-" + str(mon) + "-" + str(day)
-            seconds_of_record.append(date)  # Puts the creation time into a list
+            date_of_record.append(datetime.fromtimestamp(date).strftime('%Y-%m-%d')) # Puts the creation time into a list
             hostnames.append(hostname)  # Puts the hostname into a list
     except IndexError:  # Got to the last record in the list
         pass
 
 pd.options.display.float_format = '{:10f}'.format
-data = {"Hostname Creation Date": seconds_of_record, "Hostname": hostnames}
+data = {"Hostname Creation Date": date_of_record, "Hostname": hostnames}
 df = pd.DataFrame(data)
 df = df.sort_values(by=["Hostname Creation Date"])
 df.index = range(len(df))  # Resets the indexing after the the sorting
